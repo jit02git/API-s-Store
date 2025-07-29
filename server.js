aa83a18a708f6da84f14699aa1fe4cabd8886533
jitesh <jitesh02sep@gmail.com>
2025-07-29 22:34:24 +0530

    fixed issues

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dbConnect = require('./Config/dbConnect');
 const AuthRoutes = require('./Route/AuthRoute');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 dbConnect();
 

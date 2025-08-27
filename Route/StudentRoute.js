7733217af8e0a2cd49c65b61ab50b8958fe44032
jitesh <jitesh02sep@gmail.com>
2025-08-28 00:03:40 +0530

    routing the api's

--- Route/StudentRoute.js
+++ Route/StudentRoute.js
@@ -6,5 +6,11 @@ const validate = require('../Middleware/Validate');
 
 // POST /api/students
 studentRouter.post('/create', createStudentValidation, validate, studentController.createStudent);
+// GET /api/students
+studentRouter.get('/', studentController.getStudent);
+// PUT /api/students/:id
+studentRouter.put('/:id', studentController.updateStudent);
+// DELETE /api/students/:id
+studentRouter.delete('/:id', studentController.deleteStudent);
 
 module.exports = studentRouter;
